09dfcedc2f70aeac437851788dbe4ac93dcc9ddb
layan123-create <layanawad123123@gmail.com>
2025-03-01 20:05:22 +0200

    first commit

--- app/src/main/java/com/example/listeners/MainActivity.java
+++ app/src/main/java/com/example/listeners/MainActivity.java
@@ -1,18 +1,50 @@
 package com.example.listeners;
-
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch sw1;
+    private SeekBar bar12;
+    private ImageView pecture1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sw1 = findViewById(R.id.sw);
+        bar12 = findViewById(R.id.bar1);
+        pecture1 = findViewById(R.id.pecture);
+
+        sw1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                pecture1.setImageResource(R.drawable.happy);
+            } else {
+                pecture1.setImageResource(R.drawable.sad);
+            }
+        });
+
+        bar12.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                pecture1.setX(pecture1.getX() + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Image X ="+ pecture1.getX(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
